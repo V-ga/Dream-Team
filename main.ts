289bb0020f2fb7222ac57a59c5d5e3838bcf4272
VVEDANT123XX <85978964+VVEDANT123XX@users.noreply.github.com>
2022-05-05 11:30:00 +0530

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,4 @@
-game.splash("Welcome to Ecosystem Simulator", "Press q to continue")
-game.splash("In this game, you have to build an ecosystem for 7 days, while facing and finding solutions for various threats such as deforestation,forest fires,etc", "Be careful not to let any animals get extinct")
+game.splash("In this game, you have to build an ecosystem for 7 days, while facing and finding solutions for various threats such as deforestation,forest fires,etc. Be careful not to let any animals get extinct")
+game.splash("Day-1")
+scene.setBackgroundImage(assets.image`myImage8`)
+let lion_food = false || false
